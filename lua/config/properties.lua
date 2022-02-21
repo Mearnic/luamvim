41ac17e086441e20dd9df3317f85f6cd382d8f55
Mearnic <1335666107@qq.com>
2022-02-21 08:53:08 +0800

    add properties

--- lua/config/properties.lua
+++ lua/config/properties.lua
@@ -1,20 +1,50 @@
 
+-- 文本编码格式
 vim.g.encoding = "UTF-8"
+-- 文件编码格式
 vim.o.fileencoding = 'utf-8'
 
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- 显示行号
+vim.o.number = true
+-- 中间行为0
+vim.o.relativenumber = true
 
-vim.wo.cursorline = true
+-- 高亮聚焦行
+vim.o.cursorline = true
 
--- 左侧图标显示栏
--- vim.wo.signcolumn = "yes"
+-- 自动读取Vim外的修改
+vim.o.autoread = true
 
--- 换行参考线
+-- 编辑框左侧图标
+vim.o.signcolumn = "yes"
+
+-- 恢复文件的位置
+-- vim.o.viewoptions = "cursor,folds,slash,unix"
+
+-- 参考线
 -- vim.wo.colorcolumn = "80"
 
--- 4个空格等于一个TAB
+-- 开启tab转空格
 vim.o.expandtab = true
+-- tab为几个空格
 vim.o.tabstop = 4
+-- 格式化tab为空格的个数
 vim.o.shiftwidth = 4
+-- 几个空格为tab
 vim.o.softtabstop = 4
+-- >> << 长度
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+
+-- 允许鼠标操作
+vim.o.mouse = "a"
+
+-- 搜索大小写不敏感，除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+
+-- 不可见字符的显示，这里只把空格显示为一个点
+vim.o.list = true  
+vim.o.listchars = "tab:> ,trail:▫"
+
